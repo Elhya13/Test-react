22f1d1878fe547ccabbd908df3b3cc6c0308b611
Elhya13 <aureliewinkin@gmail.com>
2022-11-21 13:40:07 +0100

    keystuff

--- intro-react/src/components.js/list.js
+++ intro-react/src/components.js/list.js
@@ -1,14 +1,27 @@
+import React, { useState } from "react";
+
+    
 function List() {
-    const todos = ["Coffee", "Tea", "Chocolate"]
+    const [checked, setChecked] = React.useState(false);
+    const handleChange = () => {
+        setChecked(!checked);
+    };
+    const drinks = ["Coffee", "Tea", "Chocolate"]
     return (
     <ul>
-        {todos.map ((hotbeverage)=> (
+        {drinks.map ((hotbeverage)=> (
         <li key={hotbeverage}>
-            < input type="checkbox" />  {hotbeverage}
-       
+            <label>
+            < input 
+            type="checkbox" 
+            checked={checked}
+            onChange={handleChange}
+            />  {hotbeverage}
+            </label>
         </li>
         ))}
     </ul> 
     );
 }
+
 export default List;
